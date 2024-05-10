b75d84ee36726fb74c9700f9d27d576bfeb8b558
Rohit Aute <rohit.aute450@gmail.com>
2024-05-11 00:36:19 +0530

    Project Updated!!!

---
+++ src/main/java/com/jspiders/springmvc/service/CarService.java
@@ -0,0 +1,64 @@
+package com.jspiders.springmvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jspiders.springmvc.dao.CarDAO;
+import com.jspiders.springmvc.dto.CarDTO;
+import com.jspiders.springmvc.dto.UserDTO;
+
+@Component
+public class CarService {
+
+	@Autowired
+	private CarDAO carDAO;
+
+	public CarDTO addCar(String name, String brand, Double price) {
+		 CarDTO carDTO = new  CarDTO();
+		 carDTO.setName(name);
+		 carDTO.setBrand(brand);
+		 carDTO.setPrice(price);
+		 return carDAO.addCar(carDTO);
+		
+	}
+	
+	public List<CarDTO> findAllCars(){
+		
+		List<CarDTO> cars = carDAO.findAllCars();
+		if(cars !=null && cars.size()>0) {
+			return cars;
+		}else {
+			return null;
+		}
+	}
+	
+	public List<CarDTO> findAllCarsByUser(UserDTO signedInUser) {
+		List<CarDTO> cars = carDAO.findAllCarsByUser(signedInUser.getId());
+			if (cars != null && cars.size() > 0) {
+			return cars;
+		} else {
+			return null;
+		}
+	}
+
+	public void deleteCar(UserDTO signedInUser, int carId) {
+		carDAO.deleteCar(signedInUser.getId(), carId);
+	}
+
+	public void updateCar(int id, String name, String brand, double price) {
+		CarDTO car = new CarDTO();
+		car.setId(id);
+		car.setName(name);
+		car.setBrand(brand);
+		car.setPrice(price);
+		carDAO.updateCar(car);
+	}
+
+	public CarDTO findCarById(int id) {
+		return carDAO.findCarById(id);
+	}
+
+
+}
